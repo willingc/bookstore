781548042304a5174eea564603d62cc68db89cf4
Carol Willing <carolcode@willingconsulting.com>
2019-05-10 14:56:46 -0700

    update package dir and extension name

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from ._version import __version__, version_info
 from .archive import ArchiveRecord, BookstoreContentsArchiver
 from .bookstore_config import BookstoreSettings
@@ -5,6 +7,9 @@ from .clone import BookstoreCloneHandler
 from .handlers import BookstoreVersionHandler, load_jupyter_server_extension
 from .publish import BookstorePublishHandler
 
+PACKAGE_DIR = os.path.realpath(os.path.dirname(__file__))
+EXT_NAME = "bookstore"
+
 
 def _jupyter_server_extension_paths():
-    return [dict(module="bookstore")]
+    return [{"module": EXT_NAME}]
