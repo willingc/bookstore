0c7927c533dff5072e2d69ae01ccc2c526870e26
Carol Willing <carolcode@willingconsulting.com>
2019-05-09 17:41:09 -0700

    pin botocore in doc build

--- setup.py
+++ setup.py
@@ -79,7 +79,14 @@ setup(
         'botocore',
     ],
     extras_require={
-        'docs': ['sphinx>=2.0', 'recommonmark', 'm2r', 'sphinxcontrib-napoleon', 'boto3==1.9.91'],
+        'docs': [
+            'sphinx>=2.0',
+            'recommonmark',
+            'm2r',
+            'sphinxcontrib-napoleon',
+            'boto3==1.9.91',
+            'botocore>=1.12.91,<1.12.92',
+        ],
         'test': [
             'codecov',
             'coverage',
